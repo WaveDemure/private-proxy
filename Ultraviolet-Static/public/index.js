2caafd23047eab08194b5d45918b91b7cf9ebd25
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-01 21:29:39 -0600

    Add files via upload

---
+++ Ultraviolet-Static/public/index.js
@@ -0,0 +1,38 @@
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+var logID = document.getElementById("logs")
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    logID.innerHTML = logID.innerHTML + "Failed to register Service worker";
+    logID.innerHTML = logID.innerHTML + err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
