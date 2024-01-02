507023506c00853a6a015a688f7847fca8517f6a
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-02 03:33:21 -0600

    testUpdate

--- Ultraviolet-Static/public/index.js
+++ Ultraviolet-Static/public/index.js
@@ -28,11 +28,49 @@ form.addEventListener("submit", async (event) => {
   try {
     await registerSW();
   } catch (err) {
-    logID.innerHTML = logID.innerHTML + "Failed to register Service worker";
-    logID.innerHTML = logID.innerHTML + err.toString();
+    document.write("Failed to register Service worker" + err.toString() + " | <button onclick='location.reload()'>Return</button>");
     throw err;
   }
 
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
+
+document.getElementById("add-mark").addEventListener("click", function () {
+  var B_Num = prompt("Bookmark name");
+  var B_Link = prompt("Bookmark link");
+
+  var convertedJson = {
+    "name": B_Num,
+    "link": B_Link
+  }
+
+  var bookDiv = document.createElement("div");
+  document.getElementById("mark-contanier").appendChild(bookDiv);
+
+  var bookButton = document.createElement("a");
+  bookButton.innerHTML = convertedJson.name;
+  bookButton.classList.add("bookButton")
+  bookButton.id = B_Link
+  bookButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+  
+    try {
+      await registerSW();
+    } catch (err) {
+      document.write("Failed to register Service worker" + err.toString() + " | <button onclick='location.reload()'>Return</button>");
+      throw err;
+    }
+    if (bookButton.id.includes("https://")) {
+      const url = search(address.value);
+      console.log(url)
+      location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    } else {
+      const url = search(address.value, searchEngine.value);
+      console.log(url)
+      location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    }
+
+  });
+  bookDiv.appendChild(bookButton)
+})
